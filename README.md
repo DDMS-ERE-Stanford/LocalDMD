9027167fa30fb226ac3ade6489b6fb834d1a0644
Hongli (Bob) Zhao <honglizhaobob@uchicago.edu>
2024-11-25 18:52:40 -0600

    updated plots for draft revision

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ This repository contains code files for the manuscript by [Hongli Zhao and Danie
 
 The description of the repository structure is as follows. We mainly test 4 methods in dynamic mode decomposition (DMD), (1) standard DMD, (2) [Lagrangian DMD](https://arxiv.org/abs/1908.03688), (3) [Online DMD](https://arxiv.org/abs/1707.02876), (4) Local Lagrangian DMD (proposed).
 
-* `/experiments` contains several advection-dominated examples (e.g. conservative advection in 1D and 2D and 2D oscillator ODE). In order to reproduce the Navier-Stokes equation example, please refer to instructions of [this repository](https://github.com/JamieMJohns/Navier-stokes-2D-numerical-solve-incompressible-flow-with-custom-scenarios-MATLAB-) to generate necessary solution data.
+* `/experiments` contains several advection-dominated examples (e.g. conservative advection in 1D and 2D and 2D oscillator ODE). In order to reproduce the Navier-Stokes equation example, please refer to instructions of [this repository](https://github.com/JamieMJohns/Navier-stokes-2D-numerical-solve-incompressible-flow-with-custom-scenarios-MATLAB-) to generate necessary solution data (run `Main.m`, the data will be saved locally to a folder, then post-process with DMD strategies). 
 
 * `/scaling` contains a subroutine for scalably updating the singular value decomposition (SVD) in windows.
 
--- upper_bounds/test1_linear_system.m
+++ upper_bounds/test1_linear_system.m
@@ -5,15 +5,19 @@
 
 clear; clc; rng("default");
 
+
 % simulate data for the time dependent linear system
 x0 = [1,0];
 dt = 1e-3;
-tend = 10.0;
+tend = 20.0;
 tspan = 0:dt:tend+dt;
+% parameter for strength of linear dynamical system 
+eps = 0.1;
 nt = length(tspan);
-[t,U] = ode45(@linear_time, tspan, x0);
+wrapper = @(t, x) linear_time(t, x, eps);
+[t,U] = ode45(wrapper, tspan, x0);
 U = U';
-%% Time-shift upper bound
+% Time-shift upper bound
 % divide data 
 % number of snapshots
 M = 1000+1;
@@ -25,6 +29,9 @@ m = M-k+1;
 all_errors = zeros(M,1);
 all_upper_no_physics = zeros(M,1);
 all_upper_with_physics = zeros(M,1);
+
+% actual Y norm
+all_actual_y_norms = zeros(M,1);
 for i = 1:m-1
     % subslice matrices
     Xi = U(:,1:i);
@@ -56,7 +63,8 @@ for i = 1:m-1
     v_norm = norm(v,2);
     X_inv_norm = norm(pinv(Xi),2);
 
-    Y_norm = norm(Yi);
+    Y_norm = norm(Yi,2);
+    all_actual_y_norms(i) = Y_norm;
     %c = 1/(u'*u-u'*Xi*pinv(Xi)*u);
     c = 1/(u'*u);
     const1 = (c^2)*(u_norm^2)*(1+(u_norm/X_inv_norm)^2);
@@ -65,7 +73,7 @@ for i = 1:m-1
     all_upper_no_physics(i) = sqrt(const1*const2+const3);
 
     % compute physics-informed upper bound
-    Y_norm_physics = all_upper(i);
+    Y_norm_physics = all_upper(i)^2;
     % modified const2, others do not depend on Ynorm
     const2 = (Y_norm_physics^2+v_norm^2);
     all_upper_with_physics(i) = sqrt(const1*const2+const3);
@@ -73,59 +81,83 @@ for i = 1:m-1
 end
 
 
-%%
+
 figure(1);
-plot_skip=20;
+plot_skip=21;
 plot(1:plot_skip:M, log10(all_Xnorm(1:plot_skip:end)), ...
-    "-*", "Color", "green", "LineWidth", 3.0); 
+    "--", "Color", "green", "LineWidth", 5.0); 
+
+hold on;
+plot(1:plot_skip:M, log10(all_actual_y_norms(1:plot_skip:end)), ...
+    "-.", "Color", "#EDB120", "LineWidth", 5.0); 
+
 hold on;
 plot(1:plot_skip:M, log10(all_upper(1:plot_skip:end)), ...
-    "-*", "Color", "blue", "LineWidth", 3.0); 
+    "-", "Color", "black", "LineWidth", 5.0); 
 
-legend(["$||\mathbf{X}||$", "Bound on $||\mathbf{Y}||$"], "FontSize", 24, ...
-    "Location", "southeast", "Interpreter", "latex");
-xlabel("$r$", "Interpreter", "latex", "FontSize", ...
-    24, 'fontweight','bold');
-ylabel("(Log) Matrix 2-Norm", "Interpreter", "latex", "FontSize", ...
-    24, 'fontweight','bold');
+
+
+%legend(["$||\mathbf{X}||$", "$||\mathbf{Y}||$", ...
+ %   "$||\mathbf{Y}||$ bound"], "FontSize", 16, ...
+  %  "Location", "southeast", "Interpreter", "latex");
+legend({'$\|\mathbf{X}\|$', '$\|\mathbf{Y}\|$', ...
+    '$\|\mathbf{Y}\|$ bound'}, ...
+       'Interpreter', 'latex', ...
+       'Orientation', 'horizontal', ...
+       'Location', 'south',"FontSize", 24);
+legend boxoff;
+
 ax = gca;
-ax.FontSize = 24; 
-title("Effect of Time Shift")
+ax.FontSize = 20; 
+title("Effect of time shift",'Interpreter','latex');
+ytickformat('10^{%0d}');
+xlabel("$N_{sn}$", "Interpreter", "latex", 'fontweight','bold');
+ylabel("$\|\cdot\|_2$", "Interpreter", "latex", 'fontweight','bold');
+
+xticks([50 250 500 750 1000])
 exportgraphics(gcf, "./fig/time_shift_bound.png","Resolution",200);
 
 
 figure(2);
-plot(1:M-1, log10(all_upper_with_physics(1:end-1)), "--", ...
-    "Color", "blue", "LineWidth", 3.0); 
+plot(1:plot_skip:M-1, ...
+    log10(all_upper_with_physics(1:plot_skip:end-1)), "-", ...
+    "Color", "black", "LineWidth", 5.0); 
 hold on;
-plot(1:M-1, log10(all_upper_no_physics(1:end-1)), "--", ...
-    "Color", "green", "LineWidth", 3.0); 
+plot(1:plot_skip:M-1, ...
+    log10(all_upper_no_physics(1:plot_skip:end-1)), "-.", ...
+    "Color", "blue", "LineWidth", 5.0); 
 hold on;
-plot(1:M-1, log10(all_errors(1:end-1)),"LineWidth", 3.0);
-legend(["Error bound (linear system)", "Error bound", ...
+plot(1:plot_skip:M-1, ...
+    log10(all_errors(1:plot_skip:end-1)), '--',...
+    "LineWidth", 5.0,'Color','green');
+lgd = legend(["Error bound", "Error bound (physics)", ...
     "Actual perturbation"]);
-xlabel("$r$", "Interpreter", "latex", "FontSize", ...
-    24, 'fontweight','bold');
-ylabel("(Log) Matrix 2-Norm Error", "Interpreter", "latex", "FontSize", ...
-    24, 'fontweight','bold');
+lgd.Position = [0.45, 0.40, 0.4, 0.05];
+legend boxoff;
+
+ytickformat('10^{%0d}');
+xlabel("$N_{sn}$", ...
+    "Interpreter", "latex", 'fontweight','bold');
+ylabel("$\|\cdot\|_2$", "Interpreter", "latex", ...
+    'fontweight','bold');
 ax = gca;
-ax.FontSize = 24; 
-title("Updated DMD perturbation")
+ax.FontSize = 20; 
+title("Operator perturbation $\|\mathbf{K}-\mathbf{K}_{N_{sn}}\|_2$",'Interpreter','latex');
 exportgraphics(gcf, "./fig/physics_informed_perturb.png","Resolution",200);
 
 
 
 %% Helper
 % simple time dependent linear system
-function dxdt = linear_time(t, x)
+function dxdt = linear_time(t, x, eps)
     dxdt = zeros(2, 1);
-    dxdt(1) = (1+0.1*t)*x(2);
-    dxdt(2) = (-1-0.1*t)*x(1);
-    dxdt = dxdt + f(t);
+    dxdt(1) = (1+eps*t)*x(2);
+    dxdt(2) = (-1-eps*t)*x(1);
+    dxdt = dxdt + f(t,eps);
 end
 
-function feval = f(t)
+function feval = f(t, eps)
     feval = zeros(2,1);
-    feval(1)=-sin(0.1*t);
-    feval(2)=cos(0.1*t);
+    feval(1)=-sin(eps*t);
+    feval(2)=cos(eps*t);
 end
